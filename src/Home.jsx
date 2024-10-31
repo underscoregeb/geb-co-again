ca9938f7ddf880683a3f104f5b1233d89efa3383
geb <gablanjoh@icloud.com>
2024-10-31 02:49:19 +0000

    i love love love responsive images !!

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,20 +3,25 @@
 function Page() {
   return (
     <>
-        <div className='doc-content'>
-            <img src="icecream.webp" alt="Tomorrow starts yesterday." />
-            <h1>Lorem Ipsum</h1>
-            <p>
-                Ex esse ullamco consequat quis mollit Lorem tempor culpa deserunt cupidatat. Ad in eiusmod qui adipisicing. Incididunt ut ea ipsum. Pariatur ex amet cillum enim ullamco adipisicing sit ut Lorem. Reprehenderit ea et aliquip pariatur. Aute sit consequat Lorem cupidatat cupidatat consequat consectetur proident veniam. Id proident officia adipisicing ex culpa reprehenderit nulla eu. Cupidatat non laboris enim reprehenderit enim anim Lorem non.
-            </p>
-            <h2>Est elit aute ullamco qui do culpa ea.</h2>
-            <p>
-              Nisi excepteur cupidatat adipisicing occaecat voluptate dolor eiusmod magna duis occaecat et. Ut Lorem pariatur sint in qui dolore ea et qui enim officia adipisicing cupidatat proident labore. Labore deserunt deserunt aliquip. Exercitation mollit anim aute occaecat. Ad velit culpa eu sunt non elit nisi. Quis occaecat magna qui nostrud excepteur ut nostrud do voluptate eu laborum voluptate voluptate veniam.
-            </p>
-            <p>
-              Magna veniam excepteur commodo. Exercitation consequat eiusmod Lorem officia Lorem mollit cillum est. Duis veniam dolore incididunt tempor esse velit quis. Minim est enim qui et magna commodo commodo. Voluptate do ut aliqua culpa.
-            </p>
-        </div>
+      <div className='doc-content'>
+        <img
+          src="icecream-large.webp"
+          srcSet="icecream-small.webp 480w, icecream-large.webp 1080w, icecream-full.webp 3072w"
+          sizes="40em"
+          alt="A delicious ice cream cone muah"
+        />
+        <h1>Lorem Ipsum</h1>
+        <p>
+          Ex esse ullamco consequat quis mollit Lorem tempor culpa deserunt cupidatat. Ad in eiusmod qui adipisicing. Incididunt ut ea ipsum. Pariatur ex amet cillum enim ullamco adipisicing sit ut Lorem. Reprehenderit ea et aliquip pariatur. Aute sit consequat Lorem cupidatat cupidatat consequat consectetur proident veniam. Id proident officia adipisicing ex culpa reprehenderit nulla eu. Cupidatat non laboris enim reprehenderit enim anim Lorem non.
+        </p>
+        <h2>Est elit aute ullamco qui do culpa ea.</h2>
+        <p>
+          Nisi excepteur cupidatat adipisicing occaecat voluptate dolor eiusmod magna duis occaecat et. Ut Lorem pariatur sint in qui dolore ea et qui enim officia adipisicing cupidatat proident labore. Labore deserunt deserunt aliquip. Exercitation mollit anim aute occaecat. Ad velit culpa eu sunt non elit nisi. Quis occaecat magna qui nostrud excepteur ut nostrud do voluptate eu laborum voluptate voluptate veniam.
+        </p>
+        <p>
+          Magna veniam excepteur commodo. Exercitation consequat eiusmod Lorem officia Lorem mollit cillum est. Duis veniam dolore incididunt tempor esse velit quis. Minim est enim qui et magna commodo commodo. Voluptate do ut aliqua culpa.
+        </p>
+      </div>
     </>
   )
 }
