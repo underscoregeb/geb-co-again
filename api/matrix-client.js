67b0e5bb415a74bc4044e6127c5d67f526e79146
geb <95725932+underscoregeb@users.noreply.github.com>
2025-01-14 12:48:52 -0600

    Delete api directory
    moved off of vercel; no longer using this

--- api/matrix-client.js
+++
@@ -1,9 +0,0 @@
-
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow CORS
-  res.status(200).json({
-    "m.homeserver": {
-      "base_url": "https://matrix.geb.company"
-    }
-  });
-}
--- api/matrix-server.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    "m.server": "matrix.geb.company:443"
-  });
-}
